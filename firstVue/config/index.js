efb1b1983e8a5e35ebf46fa0d4b5f45c66575d58
pdrain <pdrain@yeah.net>
2017-08-07 13:48:33 +0800

    ss

--- firstVue/config/index.js
+++ firstVue/config/index.js
@@ -29,7 +29,7 @@ module.exports = {
           assetsPublicPath: '/',
           proxyTable: {
                           '/api': {
-                            target: 'http://localhost/',
+                            target: 'http://serviceapi.meilituibian.cn/',
                             changeOrigin: true
                             //pathRewrite: { '^/api': ''}
                           }
---
+++ firstVue/src/components/Dictionary.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class='hmn-dict' style="{'height':screenHeight}">
+    <span class="navi">
+      <dl>
+        <dd></dd>
+      </dl>
+    </span>
+    <span class="detail">222</span>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    screenHeight: () => {
+      return window.screen.height
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hmn-dict {
+  line-height: 35px;
+  height: 100%;
+}
+
+.navi {
+  flex: 0;
+  float: left;
+  width: 30%;
+  border-right:1px #ccc solid;
+  height: 100%;
+  background: #fff;
+  box-sizing: border-box;
+}
+
+.detail {
+  flex: 0;
+  float: left;
+  width:70%;
+  height: 100%;
+  background: #fff;
+  text-align: left;
+  padding:0px 5px;
+  box-sizing:border-box;
+
+}
+</style>
